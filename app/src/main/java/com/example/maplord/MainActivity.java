413af3b9505e02734f5e5f9b4d097a07c3e31de0
Dorin Michaeli <dorin.michaeli1@gmail.com>
2023-06-08 01:59:49 +0300

    Removed no longer required login fragment. We now have a dedicated login activity that lets users log in with their google accounts!

--- app/src/main/java/com/example/maplord/MainActivity.java
+++ app/src/main/java/com/example/maplord/MainActivity.java
@@ -12,18 +12,25 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.maplord.databinding.ActivityMainBinding;
+import com.example.maplord.services.UserService;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
   private AppBarConfiguration appBarConfiguration;
 
   // Dependencies.
   private LocationService locationService;
+  private UserService userService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     locationService = MapLordApp.get(this).getLocationService();
+    userService = MapLordApp.get(this).getUserService();
+
+    FirebaseUser user = getIntent().getParcelableExtra("user");
+    userService.setUser(user);
 
     ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
